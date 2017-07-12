3fcb9ed1abcbc19f76d977cd213dbf6ce383735f
Stephan Lensky <kidcontact@users.noreply.github.com>
2017-07-12 16:58:43 -0400

    Add files via upload

--- easyros.py
+++ easyros.py
@@ -10,15 +10,21 @@ pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDrive
 r = rospy.Rate(10) # 10hz
 
 def move(speed, steering_angle, duration):
+    # initialize the message using the arguments provided
     msg = AckermannDriveStamped()
-    msg.header.frame_id = '/base_link'
     msg.drive.speed = speed
     msg.drive.steering_angle = steering_angle
     
+    # pretty sure this is what the frame_id should be?
+    # probably doesn't really matter anyway
+    msg.header.frame_id = '/base_link'
+    
+    # continue publishing our move message until either the robot shuts down or the timer runs out
     start_time = time()
     while not rospy.is_shutdown() and start_time + duration > time():
         msg.header.stamp = rospy.Time.now()
         pub.publish(msg)
+        # makes the loop run only 10 times a second (configurable)
         r.sleep()
     
     loginfo('Done move (speed %s, steering_angle %s, duration %s)', speed, steering_angle, duration)
